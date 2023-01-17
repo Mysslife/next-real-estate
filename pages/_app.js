a6101eecc9217d43e72f137780b3cf52f380de2e
vnjvthaind <thaind@lotte.vn>
2023-01-17 14:45:39 +0700

    update: code 20230117

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,31 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import Router from "next/router";
 import Head from "next/head";
+import Router from "next/router";
 import NProgress from "nprogress";
-
+import { Fragment } from "react";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
+  NProgress.configure({ showSpinner: false })
 
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  })
+
+  return (
+    <Fragment>
+      <Head>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
-    </>
+    </Fragment>
   );
 }
--- pages/search.jsx
+++ pages/search.jsx
@@ -35,7 +35,7 @@ const Search = ({ properties }) => {
       </Text>
       <Flex flexWrap="wrap">
         {properties?.map((property) => (
-          <Property property={property} key={property.id} />
+          <Property property={ property } key={ property.id } />
         ))}
       </Flex>
       {properties?.length === 0 && (
